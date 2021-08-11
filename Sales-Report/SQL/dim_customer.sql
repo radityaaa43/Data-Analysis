5ad284b9e92c57f4eac58654ccc1d71a4edc0b03
Raditya Yoga Asditama <64305735+radityaaa43@users.noreply.github.com>
2021-08-12 03:48:54 +0700

    Add files via upload

---
+++ Sales-Report/SQL/dim_customer.sql
@@ -0,0 +1,13 @@
+SELECT 
+  c.CustomerKey, 
+  c.firstname AS FirstName, 
+  c.lastname AS LastName, 
+  c.FirstName + ' ' + c.LastName AS FullName, 
+  CASE c.Gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+  c.datefirstpurchase AS DateFirstPurchase,
+  g.City AS CustomerCity 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] c 
+  LEFT JOIN [AdventureWorksDW2019].[dbo].[DimGeography] g ON c.GeographyKey = g.GeographyKey 
+ORDER BY 
+  1 ASC
